9aa0dd26e5377a4365837fd15cf82190135cf389
Daan Bloembergen <daan.bloembergen@gmail.com>
2026-07-31 16:26:12 +0200

    Add inference pipeline

---
+++ zwerfafval_detectie/inference_pipeline/submit_inference_pipeline.py
@@ -0,0 +1,63 @@
+import os
+
+from azure.ai.ml import Input, Output
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.dsl import pipeline
+
+from zwerfafval_detectie import aml_interface, settings
+from zwerfafval_detectie.inference_pipeline.components.run_inference import (
+    run_inference,
+)
+
+
+@pipeline()
+def inference_pipeline():
+    input_datastore_path = aml_interface.get_datastore_full_path(
+        settings["inference_pipeline"]["inputs"]["datastore_path"]
+    )
+    output_datastore_path = aml_interface.get_datastore_full_path(
+        settings["inference_pipeline"]["outputs"]["datastore_path"]
+    )
+
+    inference_data_rel_path = settings["inference_pipeline"]["inputs"][
+        "inference_data_rel_path"
+    ]
+    model_weights_rel_path = settings["inference_pipeline"]["inputs"][
+        "model_weights_rel_path"
+    ]
+    output_rel_path = settings["inference_pipeline"]["outputs"]["output_rel_path"]
+
+    inference_data_path = os.path.join(input_datastore_path, inference_data_rel_path)
+    inference_data = Input(
+        type=AssetTypes.URI_FOLDER,
+        path=inference_data_path,
+    )
+
+    model_weights_path = os.path.join(input_datastore_path, model_weights_rel_path)
+    model_weights = Input(
+        type=AssetTypes.URI_FOLDER,
+        path=model_weights_path,
+    )
+    run_inference_step = run_inference(
+        inference_data_dir=inference_data, model_weights_dir=model_weights
+    )
+
+    output_path = os.path.join(output_datastore_path, output_rel_path)
+    run_inference_step.outputs.output_dir = Output(
+        type="uri_folder", mode="rw_mount", path=output_path
+    )
+
+    return {}
+
+
+def main() -> None:
+    aml_interface.submit_pipeline_experiment(
+        pipeline_function=inference_pipeline,
+        experiment_name=settings["aml_experiment_details"]["experiment_name"],
+        default_compute=settings["aml_experiment_details"]["compute_name"],
+        show_log=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- zwerfafval_detectie/settings/settings_schema.py
+++ zwerfafval_detectie/settings/settings_schema.py
@@ -1,34 +1,11 @@
-from typing import Any, Dict, List, Union
+from typing import Dict, List, Union
 
-from pydantic import BaseModel
-
-
-class SettingsSpecModel(BaseModel):
-    class Config:
-        extra = "forbid"
-
-
-class AMLExperimentDetailsSpec(SettingsSpecModel):
-    experiment_name: str
-    compute_name: str = None
-    env_name: str = None
-    env_version: int = None
-    src_dir: str = None
-    ai_instrumentation_key: str = None
-
-
-class LoggingSpec(SettingsSpecModel):
-    loglevel_own: str = "INFO"
-    own_packages: List[str] = [
-        "__main__",
-    ]
-    extra_loglevels: Dict[str, str] = {}
-    basic_config: Dict[str, Any] = {
-        "level": "WARNING",
-        "format": "%(asctime)s|%(levelname)-8s|%(name)s|%(message)s",
-        "datefmt": "%Y-%m-%d %H:%M:%S",
-    }
-    ai_instrumentation_key: str = ""
+from yolo_model_development_kit.settings.settings_schema import (
+    AMLExperimentDetailsSpec,
+    InferencePipelineSpec,
+    LoggingSpec,
+    SettingsSpecModel,
+)
 
 
 class TrainingModelParameters(SettingsSpecModel):
@@ -57,6 +34,7 @@ class ZwerfafvalDetectieSettingsSpec(SettingsSpecModel):
         extra = "forbid"
 
     customer: str
-    aml_experiment_details: AMLExperimentDetailsSpec
     logging: LoggingSpec = LoggingSpec()
+    aml_experiment_details: AMLExperimentDetailsSpec
+    inference_pipeline: InferencePipelineSpec
     training_pipeline: TrainingPipelineSpec = None
